2d5c52f6bc2744bcbd36938fb0e3a9b9c73b9584
Leung Cheng <monewckl@hotmail.com.hk>
2024-10-24 23:18:10 +0800

    Import project generated by Spring Initializr

---
+++ src/test/java/com/leungcheng/spring_simple_backend/SpringSimpleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leungcheng.spring_simple_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
