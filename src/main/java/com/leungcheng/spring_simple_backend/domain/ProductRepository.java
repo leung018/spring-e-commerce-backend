104b3d577f546617f17eba1a8449e0d00c771c65
Leung Cheng <monewckl@hotmail.com.hk>
2024-10-29 22:54:41 +0800

    Add ProductRepository

---
+++ src/main/java/com/leungcheng/spring_simple_backend/domain/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.leungcheng.spring_simple_backend.domain;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductRepository extends PagingAndSortingRepository<Product, String>, CrudRepository<Product, String> {
+}
