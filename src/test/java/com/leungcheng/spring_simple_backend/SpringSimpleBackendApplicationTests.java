d857394e85107a2d270b4d97e918cdacc1fa1bef
Leung Cheng <monewckl@hotmail.com.hk>
2024-10-30 18:06:39 +0800

    Test behavior of getting product

--- src/test/java/com/leungcheng/spring_simple_backend/SpringSimpleBackendApplicationTests.java
+++ src/test/java/com/leungcheng/spring_simple_backend/SpringSimpleBackendApplicationTests.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -17,9 +19,16 @@ class SpringSimpleBackendApplicationTests {
 	private MockMvc mockMvc;
 
 	@Test
-	public void shouldCreateProduct() throws Exception {
-	 	mockMvc.perform(post("/products")
+	public void shouldCreateAndGetProduct() throws Exception {
+	 	MvcResult mvcResult = mockMvc.perform(post("/products")
 				.content("{\"name\": \"Product 1\", \"price\": 1.0, \"quantity\": 50}"))
-				.andExpect(status().isCreated());
+				.andExpect(status().isCreated())
+				.andReturn();
+		String location = mvcResult.getResponse().getHeader("Location");
+        mockMvc.perform(get(location))
+				.andExpect(status().isOk())
+			 	.andExpect(jsonPath("$.name").value("Product 1"))
+			 	.andExpect(jsonPath("$.price").value(1.0))
+			 	.andExpect(jsonPath("$.quantity").value(50));
 	}
 }
