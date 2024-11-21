bbccc4769ce02004f219b3707ecbc591878e286b
Leung Cheng <monewckl@hotmail.com.hk>
2024-11-21 21:57:06 +0800

    Add expiration

--- src/main/java/com/leungcheng/spring_simple_backend/domain/JwtService.java
+++ src/main/java/com/leungcheng/spring_simple_backend/domain/JwtService.java
@@ -1,34 +1,62 @@
 package com.leungcheng.spring_simple_backend.domain;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
 public class JwtService {
   public record UserInfo(String userId) {}
 
-  public record Config(String hs256Key) {}
+  public record Config(String hs256Key, Duration expiredDuration) {}
+
+  public static class InvalidTokenException extends RuntimeException {
+    public InvalidTokenException(String message) {
+      super(message);
+    }
+  }
 
   public JwtService(Config config) {
     byte[] keyBytes = config.hs256Key().getBytes(StandardCharsets.UTF_8);
     secretKey = new SecretKeySpec(keyBytes, "HmacSHA256");
+    expiredDuration = config.expiredDuration();
   }
 
   private final SecretKey secretKey;
 
+  private final Duration expiredDuration;
+
+  private Date getExpirationDate() {
+    return Date.from(Instant.now().plus(this.expiredDuration));
+  }
+
   public String generateAccessToken(User user) {
-    return Jwts.builder().subject(user.getId()).signWith(this.secretKey, Jwts.SIG.HS256).compact();
+    return Jwts.builder()
+        .subject(user.getId())
+        .expiration(getExpirationDate())
+        .signWith(this.secretKey, Jwts.SIG.HS256)
+        .compact();
   }
 
   public UserInfo parseAccessToken(String token) {
-    String userId =
-        Jwts.parser()
-            .verifyWith(this.secretKey)
-            .build()
-            .parseSignedClaims(token)
-            .getPayload()
-            .getSubject();
-    return new UserInfo(userId);
+    try {
+      String userId =
+          Jwts.parser()
+              .verifyWith(this.secretKey)
+              .build()
+              .parseSignedClaims(token)
+              .getPayload()
+              .getSubject();
+      return new UserInfo(userId);
+    } catch (Exception e) {
+      if (e instanceof ExpiredJwtException) {
+        throw new InvalidTokenException("Expired token");
+      }
+      throw new InvalidTokenException("Invalid token");
+    }
   }
 }
--- src/test/java/com/leungcheng/spring_simple_backend/domain/JwtServiceTest.java
+++ src/test/java/com/leungcheng/spring_simple_backend/domain/JwtServiceTest.java
@@ -1,8 +1,10 @@
 package com.leungcheng.spring_simple_backend.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import io.jsonwebtoken.Jwts;
+import java.time.Duration;
 import org.junit.jupiter.api.Test;
 
 public class JwtServiceTest {
@@ -12,9 +14,15 @@ public class JwtServiceTest {
 
   private static class JwtServiceBuilder {
     private final String hs256Key = Jwts.SIG.HS256.key().build().toString();
+    private Duration expiredDuration = Duration.ofHours(1);
+
+    private JwtServiceBuilder expiredDuration(Duration expiredDuration) {
+      this.expiredDuration = expiredDuration;
+      return this;
+    }
 
     private JwtService build() {
-      return new JwtService(new JwtService.Config(hs256Key));
+      return new JwtService(new JwtService.Config(hs256Key, expiredDuration));
     }
   }
 
@@ -27,5 +35,21 @@ public class JwtServiceTest {
     JwtService.UserInfo userInfo = jwtService.parseAccessToken(token);
 
     assertEquals(user.getId(), userInfo.userId());
+  }
+
+  @Test
+  void shouldThrowExceptionIfTokenExpired() {
+    JwtService jwtService = new JwtServiceBuilder().expiredDuration(Duration.ofSeconds(-1)).build();
+    User user = userBuilder().build();
+
+    String token = jwtService.generateAccessToken(user);
+
+    JwtService.InvalidTokenException exception =
+        assertThrows(
+            JwtService.InvalidTokenException.class,
+            () -> {
+              jwtService.parseAccessToken(token);
+            });
+    assertEquals("Expired token", exception.getMessage());
   }
 }
