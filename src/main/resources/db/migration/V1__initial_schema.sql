76e8da5f363b97c5c8946d9a20a7efd65bb5f527
Leung Cheng <monewckl@hotmail.com.hk>
2024-12-13 22:25:44 +0800

    Format by spotless

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -1,5 +1,60 @@
-create table orders (buyer_user_id varchar(255), id varchar(255) not null, request_id varchar(255), primary key (id), unique (request_id, buyer_user_id));
-create table products (price numeric(19,10) check (price>=0), quantity integer not null check (quantity>=0), id varchar(255) not null, name varchar(255) not null, user_id varchar(255), primary key (id));
-create table purchase_items (quantity integer, order_id varchar(255) not null, product_id varchar(255) not null, primary key (order_id, product_id));
-create table users (balance numeric(19,10) check (balance>=0), id varchar(255) not null, password varchar(255) not null, username varchar(255) not null unique, primary key (id));
-alter table if exists purchase_items add constraint FKdcec4w10xea9gqcsp3wqns9tc foreign key (order_id) references orders;
+CREATE
+    TABLE
+        orders(
+            buyer_user_id VARCHAR(255),
+            id VARCHAR(255) NOT NULL,
+            request_id VARCHAR(255),
+            PRIMARY KEY(id),
+            UNIQUE(
+                request_id,
+                buyer_user_id
+            )
+        );
+
+CREATE
+    TABLE
+        products(
+            price NUMERIC(
+                19,
+                10
+            ) CHECK(
+                price >= 0
+            ),
+            quantity INTEGER NOT NULL CHECK(
+                quantity >= 0
+            ),
+            id VARCHAR(255) NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            user_id VARCHAR(255),
+            PRIMARY KEY(id)
+        );
+
+CREATE
+    TABLE
+        purchase_items(
+            quantity INTEGER,
+            order_id VARCHAR(255) NOT NULL,
+            product_id VARCHAR(255) NOT NULL,
+            PRIMARY KEY(
+                order_id,
+                product_id
+            )
+        );
+
+CREATE
+    TABLE
+        users(
+            balance NUMERIC(
+                19,
+                10
+            ) CHECK(
+                balance >= 0
+            ),
+            id VARCHAR(255) NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            username VARCHAR(255) NOT NULL UNIQUE,
+            PRIMARY KEY(id)
+        );
+
+ALTER TABLE
+    IF EXISTS purchase_items ADD CONSTRAINT FKdcec4w10xea9gqcsp3wqns9tc FOREIGN KEY(order_id) REFERENCES orders;
